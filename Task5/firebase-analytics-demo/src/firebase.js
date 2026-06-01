93e3a24650917da08ea7bde9df0306e2fd891d92
Riya Bansal <riyabnsl2004@gmail.com>
2026-06-01 20:05:29 +0530

    Updates

--- Task5/firebase-analytics-demo/src/firebase.js
+++ Task5/firebase-analytics-demo/src/firebase.js
@@ -2,15 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getRemoteConfig } from "firebase/remote-config";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBhfW9yZk7R6titOPXPblTJik3AnfwEQ1Y",
-  authDomain: "fir-web-app-171b8.firebaseapp.com",
-  projectId: "fir-web-app-171b8",
-  storageBucket: "fir-web-app-171b8.firebasestorage.app",
-  messagingSenderId: "464309625818",
-  appId: "1:464309625818:web:3b62d6cd9119262725d6e3",
-  measurementId: "G-9VGR6L71GB"
-};
+
 
 const app = initializeApp(firebaseConfig);
 
