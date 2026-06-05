a209df87af356165ceb11649a4627d86e0c49318
Riya Bansal <riyabnsl2004@gmail.com>
2026-06-05 19:45:16 +0530

    Remove Task8 from TVF repository

--- Task8/bank-statement-digitizer/functions/index.js
+++
@@ -1,8 +0,0 @@
-const { setGlobalOptions } = require("firebase-functions");
-const { onRequest } = require("firebase-functions/https");
-
-setGlobalOptions({ maxInstances: 10 });
-
-exports.helloWorld = onRequest((req, res) => {
-  res.send("Hello from Bank Statement Digitizer!");
-});
--- Task8/bank-statement-digitizer/src/pages/Upload.jsx
+++
@@ -1,5 +0,0 @@
-import UploadForm from "../components/UploadForm";
-
-export default function Upload() {
-  return <UploadForm />;
-}
